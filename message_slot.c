b3c130ef65a042b0d91dff0bca5ab92c678e3fb8
shaharg <shaharg@example.com>
2024-07-17 23:26:55 +0300

    feat: working

--- message_slot.c
+++ message_slot.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+// #include <linux/fs/filemap.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/string.h>
@@ -14,8 +15,25 @@ MODULE_LICENSE("GPL");
 
 #include "message_slot.h"
 
-static message_channel *message_slots[255];
+static message_channel *message_slots[MAX_MINOR_NUM];
 
+// Helper linked list functions:
+
+int free_list(message_channel *head)
+{
+    message_channel *curr = head;
+    message_channel *next;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        kfree(curr);
+        curr = next;
+    }
+
+    head = NULL;
+    return 0;
+}
 // Device Functions
 
 static int device_open(struct inode *inode, struct file *file)
@@ -25,26 +43,85 @@ static int device_open(struct inode *inode, struct file *file)
 
 static int device_release(struct inode *inode, struct file *file)
 {
-    return 0;
+    int minor_num = iminor(inode);
+    return free_list(message_slots[minor_num]);
 }
 
-static ssize_t device_read(struct file *file, char __user *buffer, size_t, length, loff_t *offset)
+static ssize_t device_read(struct file *file, char __user *buffer, size_t length, loff_t *offset)
 {
-    return -EINVAL;
+    message_channel *channel = (message_channel *)file->private_data;
+
+    if (channel == NULL || channel->message == NULL)
+        return -EWOULDBLOCK;
+
+    if (channel->length > length)
+        return -ENOSPC;
+
+    return length - copy_to_user(buffer, channel->message, length);
 }
 
-static ssize_t device_write(struct file *file, char __user *buffer, size_t, length, loff_t *offset)
+static ssize_t device_write(struct file *file, const char __user *buffer, size_t length, loff_t *offset)
 {
-    return 0;
+    int bytes_written;
+    message_channel *channel = (message_channel *)file->private_data;
+
+    if (channel == NULL)
+        return -EINVAL;
+
+    if (length == 0 || length > BUF_SIZE)
+        return -EMSGSIZE;
+
+    bytes_written = length - copy_from_user(channel->message, buffer, length);
+    channel->length = bytes_written;
+    return bytes_written;
 }
 
 static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned long ioctl_param)
 {
+    int minor_num;
+    message_channel *channel, *curr, *prev;
     if (ioctl_command_id != MSG_SLOT_CHANNEL)
     {
-        printk("some some some \n");
-        return EINVAL;
+        printk("ERROR: ioctl command id is not valid \n");
+        return -EINVAL;
     }
+
+    if (ioctl_param == 0)
+    {
+        printk("ERROR: channel id is not valid \n");
+        return -EINVAL;
+    }
+
+    minor_num = iminor(file_inode(file));
+
+    curr = message_slots[minor_num];
+    prev = curr;
+
+    while (curr != NULL)
+    {
+        if (curr->channel_id == minor_num)
+        {
+            file->private_data = curr;
+            return 0;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    channel = (message_channel *)kmalloc(sizeof(message_channel), GFP_KERNEL);
+    channel->channel_id = minor_num;
+    channel->next = NULL;
+
+    if (prev == NULL)
+    {
+        prev = channel;
+        file->private_data = prev;
+        return 0;
+    }
+
+    prev->next = channel;
+    file->private_data = channel;
+
     return 0;
 }
 
@@ -58,3 +135,43 @@ struct file_operations Fops = {
     .unlocked_ioctl = device_ioctl,
     .release = device_release,
 };
+
+static int __init simple_init(void)
+{
+    int rc = -1, i;
+
+    // Register driver capabilities. Obtain major num
+    rc = register_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME, &Fops);
+
+    // Negative values signify an error
+    if (rc < 0)
+    {
+        printk(KERN_ERR "%s registraion failed for  %d\n", DEVICE_FILE_NAME, MAJOR_NUM);
+        return rc;
+    }
+
+    printk("heyeyeeyeyyeyeyeyeyeyafaefaeh");
+    for (i = 0; i < MAX_MINOR_NUM; i++)
+    {
+        message_slots[i] = NULL;
+    }
+
+    return 0;
+}
+
+//---------------------------------------------------------------
+static void __exit simple_cleanup(void)
+{
+    int i;
+    // Unregister the device
+    // Should always succeed
+    unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
+    for (i = 0; i < MAX_MINOR_NUM; i++)
+    {
+        free_list(message_slots[i]);
+    }
+}
+
+//---------------------------------------------------------------
+module_init(simple_init);
+module_exit(simple_cleanup);
--- message_slot.h
+++ message_slot.h
@@ -8,12 +8,13 @@
 #define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
 
 #define DEVICE_RANGE_NAME "message_slot"
-#define BUF_LEN 128
+#define BUF_SIZE 128
 #define DEVICE_FILE_NAME "message_slot_ss"
+#define MAX_MINOR_NUM 256
 
 typedef struct message_channel
 {
-    char[BUF_LEN] message;
+    char message[BUF_SIZE];
     int length;
     int channel_id;
     struct message_channel *next;
